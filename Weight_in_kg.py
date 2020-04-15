6665356867826e3d35f6f7982292cdb3669d11b3
ArnavPalkhiwala <palkhiwala.arnav@gmail.com>
2020-04-15 16:06:24 -0700

    Python Practice Continued

---
+++ Weight_in_kg.py
@@ -0,0 +1,3 @@
+lbs_weight = input("What is your weight? ")
+kg_weight = float(lbs_weight) * .45
+print("Your weight in kg is " + str(kg_weight))
---
+++ YearAndAge.py
@@ -0,0 +1,3 @@
+by = input("Birth Year: ")
+age = 2020-int(by)
+print("You are " + str(age) + " years old")
